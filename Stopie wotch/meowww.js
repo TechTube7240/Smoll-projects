90d0789b1a986833c2c00de89d2a4c05e37f5f6c
TechTube7240 <tesla8950@gmail.com>
2026-09-02 22:44:06 +0500

    made a stoop wotch :<

---
+++ Stopie wotch/meowww.js
@@ -0,0 +1,42 @@
+const stopwatch = document.getElementById("stopwatch");
+let startBtn = document.getElementById("startBtn");
+
+let timer = null;
+let isRunning = false;
+let startTime = 0;
+let elapsedTime = 0;
+
+function start(){
+    console.log(stopwatch.textContent)
+    if (!isRunning){
+        timer = setInterval(update, 10);
+        startTime = Date.now() - elapsedTime;
+        isRunning = true;
+    }
+}
+function stop(){
+    if (isRunning){
+        isRunning = false;
+        clearInterval(timer);
+    }
+}
+function reset(){
+    stopwatch.textContent = "00:00:00:00"
+}
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime /( 1000 * 3600));
+    hours = String(hours).padStart(2, "0");
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    minutes = String(minutes).padStart(2, "0");
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    seconds = String(seconds).padStart(2, "0");
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    stopwatch.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
+
